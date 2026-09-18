919d6c985ebe1077427349d2023f71e83d87cad3
Tahiana-Fenosoa <rasamytahiana@gmail.com>
2026-09-18 13:18:20 +0300

    feat: negatifPostitif

--- negatifPositif.js
+++ negatifPositif.js
@@ -1,5 +1,28 @@
 // exo 1
 
 function negatifPositif(arr){
-    
+    const negatif = [];
+    const positif = [];
+    const finalArray = [];
+
+
+    for(let i = 0 ; i <= arr.lenght - 1 ; i++){
+        if(arr[i] <= 0){
+            negatif.push(arr[i]);
+        }else{
+            positif.push(arr[i]);
+        }
+    }
+
+    for(let i = 0 ; i <= negatif.lenght - 1 ; i++){
+        finalArray.push(arr[i]);
+    }
+
+    for(let i = 0 ; i <= positif.lenght - 1 ; i++){
+        finalArray.push(arr[i]);
+    }
+
+    return finalArray;
 }
+
+console.log(negatifPositif([[3, -1, 0, 4, -2, 5, -3]]));
