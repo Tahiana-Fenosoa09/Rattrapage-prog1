f585c82918cde13bf0706e91315c58b52b9285b7
Tahiana-Fenosoa <rasamytahiana@gmail.com>
2026-09-18 14:56:58 +0300

    feat: add ranger.js

---
+++ ranger.js
@@ -0,0 +1,13 @@
+// exo 2 , 2.2
+
+// ANALYSE : 
+// entrées : tableau de nombre (0 et 1) désordonnée
+// sorties : tableau de nombre ordonnées dans l'order croissant , plus petit au plus grand 
+
+// CONCEPTION : 
+// - Itérer sur le tableau
+// - Prendre le premier élément , le comparer
+
+export function ranger(T){
+    
+}
